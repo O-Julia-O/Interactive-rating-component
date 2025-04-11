4def64024aa78dde31fbbab12132ff3362baa563
O-Julia-O <julia.qq.onix@gmail.com>
2025-04-12 01:34:15 +0300

    simple logic of sending the rating number to the next screen

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const ratingButtons = document.querySelectorAll('.feedback-card__rating-btn');
 const submitBtn = document.getElementById("submit-btn");
 const ratingScreen = document.getElementById("rating-screen");
 const thankYouScreen = document.getElementById("thank-you-screen");
+const selectedRatingSpan = document.getElementById("selected-rating");
+
+let selectedRating = null;
 
 ratingButtons.forEach(button => {
     button.addEventListener('click', () => {
       ratingButtons.forEach(btn => btn.classList.remove('selected'));
 
       button.classList.add('selected');
+      selectedRating = button.textContent;
 });
 });
 
 submitBtn.addEventListener('click', () => {
-    ratingScreen.style.display = "none";
-    thankYouScreen.style.display = "flex";
+    if (selectedRating) {
+        selectedRatingSpan.textContent = selectedRating;
+        ratingScreen.style.display = "none";
+        thankYouScreen.style.display = "flex";
+    } else {
+        alert('Please select a rating before submitting.');
+    }
 });
